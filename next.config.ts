cf5deb79254580f078fe187dc8d70a8372be6d4d
Daniel dos Santos Elidio <danielidio@192.168.68.112>
2026-01-16 17:38:07 -0300

    Wire up custom MY_AWS keys properly

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   env: {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    MY_AWS_KEY: process.env.MY_AWS_KEY,
+    MY_AWS_SECRET: process.env.MY_AWS_SECRET,
     OPLAB_API_KEY: process.env.OPLAB_API_KEY,
   },
 };
