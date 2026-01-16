945ec32f1550406f1f72a083aab09d6265965513
Daniel dos Santos Elidio <danielidio@192.168.68.112>
2026-01-16 15:00:48 -0300

    Configurar permissao DynamoDB via codigo

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,7 +5,25 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// Grant permission to access the external DynamoDB table
+const externalTableArn = 'arn:aws:dynamodb:sa-east-1:*:table/AppControleOpcoes';
+
+backend.compute.resources.lambda.addToRolePolicy(
+  {
+    Action: [
+      'dynamodb:PutItem',
+      'dynamodb:GetItem',
+      'dynamodb:UpdateItem',
+      'dynamodb:DeleteItem',
+      'dynamodb:Query',
+      'dynamodb:Scan'
+    ],
+    Resource: [externalTableArn, `${externalTableArn}/index/*`],
+    Effect: 'Allow',
+  } as any
+);
