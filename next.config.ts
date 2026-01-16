9008243a010036bb54d15be913a73b0681d4b09e
Daniel dos Santos Elidio <danielidio@192.168.68.112>
2026-01-16 17:03:29 -0300

    Force env vars via next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    OPLAB_API_KEY: process.env.OPLAB_API_KEY,
+  },
 };
 
 export default nextConfig;
